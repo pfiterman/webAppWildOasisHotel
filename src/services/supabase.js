661b540de80208f8d1208c36f3e8c5fae58c0387
Pablo Fiterman <pfiterman@gmail.com>
2025-10-23 16:12:52 -0400

    add: .env variables

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
-export const supabaseURL = "https://bwkwzxggtcracfbnqfrd.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3a3d6eGdndGNyYWNmYm5xZnJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4MTAxNDUsImV4cCI6MjA0MTM4NjE0NX0.pHFY0GhBEZSlY1iY0Lr2P-2Tsc9qsqsiDBGhjYZyAvE";
+export const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
+
 const supabase = createClient(supabaseURL, supabaseKey);
 
 export default supabase;
